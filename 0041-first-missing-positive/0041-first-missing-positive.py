44ba7b11d149065c50297d19fa1ee641ea88eb85
Shivam Haldankar <shivamhaldankar2006@gmail.com>
2026-08-31 19:46:45 +0530

    Time: 21 ms (95.92%), Space: 26.2 MB (8.3%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        s = set(nums)
+        i = 1
+        while i in s:
+            i += 1
+        return i
+        
+        
